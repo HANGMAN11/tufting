80f45345a938267b49f6991e17c02e66498bf865
Hangman <oleja.229@gmail.com>
2025-02-19 23:20:27 +0200

    full layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+const menuBtn = document.getElementById("menu__btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = document.getElementById("i")
+
+menuBtn.addEventListener('click', (e)=>{
+    navLinks.classList.toggle('open');
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line": "ri-menu-line")
+})
+
+
+navLinks.addEventListener("click", (e)=>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line")
+})
+
+document.addEventListener('click', (e)=>{
+    if(!navLinks.contains(e.target) && !menuBtn.contains(e.target)){
+        navLinks.classList.remove("open")
+    }
+})
+
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
@@ -18,3 +41,36 @@ ScrollReveal().reveal(".header__container a", {
     delay: 1000
 });
 
+new Swiper('.card-wrapper', {
+   
+    loop: true,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+        1700:{
+            slidesPerView: 4
+        }
+    }
+  });
+  
